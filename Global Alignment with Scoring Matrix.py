c6fa3a961154ae0553ef02801ea9b61c8d64ff97
Arad Maleki <arad.m.t02@gmail.com>
2023-11-01 09:25:25 +0330

    Added solution for "Global Alignment with Scoring Matrix"

---
+++ Global Alignment with Scoring Matrix.py
@@ -0,0 +1,56 @@
+BLOSUM62 = '''   A  C  D  E  F  G  H  I  K  L  M  N  P  Q  R  S  T  V  W  Y
+              A  4  0 -2 -1 -2  0 -2 -1 -1 -1 -1 -2 -1 -1 -1  1  0  0 -3 -2
+              C  0  9 -3 -4 -2 -3 -3 -1 -3 -1 -1 -3 -3 -3 -3 -1 -1 -1 -2 -2
+              D -2 -3  6  2 -3 -1 -1 -3 -1 -4 -3  1 -1  0 -2  0 -1 -3 -4 -3
+              E -1 -4  2  5 -3 -2  0 -3  1 -3 -2  0 -1  2  0  0 -1 -2 -3 -2
+              F -2 -2 -3 -3  6 -3 -1  0 -3  0  0 -3 -4 -3 -3 -2 -2 -1  1  3
+              G  0 -3 -1 -2 -3  6 -2 -4 -2 -4 -3  0 -2 -2 -2  0 -2 -3 -2 -3
+              H -2 -3 -1  0 -1 -2  8 -3 -1 -3 -2  1 -2  0  0 -1 -2 -3 -2  2
+              I -1 -1 -3 -3  0 -4 -3  4 -3  2  1 -3 -3 -3 -3 -2 -1  3 -3 -1
+              K -1 -3 -1  1 -3 -2 -1 -3  5 -2 -1  0 -1  1  2  0 -1 -2 -3 -2
+              L -1 -1 -4 -3  0 -4 -3  2 -2  4  2 -3 -3 -2 -2 -2 -1  1 -2 -1
+              M -1 -1 -3 -2  0 -3 -2  1 -1  2  5 -2 -2  0 -1 -1 -1  1 -1 -1
+              N -2 -3  1  0 -3  0  1 -3  0 -3 -2  6 -2  0  0  1  0 -3 -4 -2
+              P -1 -3 -1 -1 -4 -2 -2 -3 -1 -3 -2 -2  7 -1 -2 -1 -1 -2 -4 -3
+              Q -1 -3  0  2 -3 -2  0 -3  1 -2  0  0 -1  5  1  0 -1 -2 -2 -1
+              R -1 -3 -2  0 -3 -2  0 -3  2 -2 -1  0 -2  1  5 -1 -1 -3 -3 -2
+              S  1 -1  0  0 -2  0 -1 -2  0 -2 -1  1 -1  0 -1  4  1 -2 -3 -2
+              T  0 -1 -1 -1 -2 -2 -2 -1 -1 -1 -1  0 -1 -1 -1  1  5  0 -2 -2
+              V  0 -1 -3 -2 -1 -3 -3  3 -2  1  1 -3 -2 -2 -3 -2  0  4 -3 -1
+              W -3 -2 -4 -3  1 -2 -2 -3 -3 -2 -1 -4 -4 -2 -3 -3 -2 -3 11  2
+              Y -2 -2 -3 -2  3 -3  2 -1 -2 -1 -1 -2 -3 -1 -2 -2 -2 -1  2  7'''
+
+scoring_matrix = {}
+for i, line in enumerate(BLOSUM62.splitlines()):
+    line = line.strip()
+    if i == 0:
+        keys = line.split()
+    else:
+        values = line.split()
+        scoring_matrix[values[0]] = {keys[j]: int(values[j + 1]) for j in range(len(keys))}
+
+string1 = ""
+input()
+while True:
+    s = input()
+    if s[0] == '>':
+        break
+    string1 += s
+string2 = ""
+while True:
+    s = input()
+    if not s:
+        break
+    string2 += s
+
+n = len(string1)
+m = len(string2)
+
+distance_matrix = [[-5 * (i + j) for i in range(m + 1)] for j in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        distance_matrix[i][j] = max(distance_matrix[i - 1][j] - 5, distance_matrix[i][j - 1] - 5,
+                                    distance_matrix[i - 1][j - 1] + scoring_matrix[string1[i - 1]][string2[j - 1]])
+
+print(distance_matrix[n][m])
